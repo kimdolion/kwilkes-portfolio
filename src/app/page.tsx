a7a0001831d4ecf98c0ac20219906e791823cdcb
Kimberly Wilkes <kimowilkes@gmail.com>
2024-12-10 00:20:45 -0500

    Update css and add a joke generator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [joke, setJoke] = useState("");
+
+  const fetchJoke = async () => {
+    try {
+      const response = await fetch(
+        "https://official-joke-api.appspot.com/random_joke",
+      );
+      const data = await response.json();
+      setJoke(`${data.setup} - ${data.punchline}`);
+    } catch (error) {
+      console.error("Error fetching joke:", error);
+    }
+  };
+
   return (
     <div className="home-page">
       <Image
@@ -14,8 +30,8 @@ export default function Home() {
       />
       <div className="home-container">
         <p>
-          Hey y'all! My name is Kimberly Wilkes. Welcome to my portfolio
-          project!
+          My name is Kimberly Wilkes. <br />
+          Welcome to my portfolio project!
         </p>
         <Link className="link" href="/about-me">
           About Me
@@ -26,6 +42,13 @@ export default function Home() {
         <Link className="link" href="/village-values">
           Village Values
         </Link>
+        <div className="joke-container">
+          <h4>Random Joke Generator</h4>
+          <p>{joke}</p>
+          <button className="button" onClick={fetchJoke}>
+            Get {joke === "" ? "A" : "Another"} Joke
+          </button>
+        </div>
       </div>
     </div>
   );
